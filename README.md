52fb52dcaf3a38268467bc0f8768173716227aea
Max Claus Nunes <maxcnunes@gmail.com>
2015-09-05 16:36:17 -0300

    configures hot reloading

--- README.md
+++ README.md
@@ -12,7 +12,3 @@ npm run start-webpack
 # second shell window
 npm run start-electron
 ```
-
-> It is pretty ugly running in different windows. But we will improve it later.
-
-Every change in the code will trigger the webpack to build the code. Use `Cmd+R` to reload the app.
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -8,25 +8,39 @@ module.exports = {
   },
   entry: {
     app: [
+      'webpack/hot/dev-server',
       './src/entry.jsx' // Your appʼs entry point
     ]
   },
   output: {
-    path: path.join(__dirname, 'public'),
-    publicPath: '/public/',
+    path: path.join(__dirname, '/public/built'),
+    publicPath: 'http://localhost:8080/built/',
     filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '/public'),
+    publicPath: 'http://localhost:8080/built/'
   },
   module: {
     loaders: [
-      {test: /\.jsx?$/, exclude: /node_modules/, loaders:
-        ['babel-loader?stage=0&optional=runtime']},
-
-      {test: /\.less$/, loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!less-loader'},
-
-      {test: /\.(?:eot|ttf|woff2?)$/, loader: 'file-loader?name=[path][name]-[hash:6].[ext]&context=assets'}
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader?stage=0&optional=runtime']
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!autoprefixer-loader?{browsers:["last 2 version"]}!less-loader'
+      },
+      {
+        test: /\.(?:eot|ttf|woff2?)$/,
+        loader: 'file-loader?name=[path][name]-[hash:6].[ext]&context=assets'
+      }
     ]
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.IgnorePlugin(new RegExp("^(fs|ipc)$")),
     new webpack.ProvidePlugin({
         Radium: 'radium',
         "_": 'lodash',
