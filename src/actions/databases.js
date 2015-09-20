a184eb5acb6ac91b5ebfb5b7ca3aea737e57ad4b
Max Claus Nunes <maxcnunes@gmail.com>
2015-09-20 20:19:00 -0300

    Organize db session
    Moves db’s main process code to a proper file. And makes actions not aware of main process.

--- src/actions/databases.js
+++ src/actions/databases.js
@@ -1,4 +1,4 @@
-const getDB = require('remote').require('./src/db').getDB;
+import { getDBSession } from '../db';
 
 
 import {
@@ -12,14 +12,14 @@ export function loadDatabases() {
   return dispatch => {
     dispatch({ type: LOAD_DATABASES_REQUEST });
 
-    return getDB().then(async function (db) {
-      const databases = (await db.databaseList()).map(name => {
+    return getDBSession().then(async function (dbSession) {
+      const databases = (await dbSession.databaseList()).map(name => {
         return { name, tables: [] };
       });
 
       // TODO: get default db from connection configuration
       // for while considers the first db the defualt db
-      databases[0].tables = await db.tableList();
+      databases[0].tables = await dbSession.tableList();
 
       dispatch({ type: LOAD_DATABASES_SUCCESS, databases });
     }).catch(error => dispatch({ type: LOAD_DATABASES_FAILURE, error }));
--- src/db/index.js
+++ src/db/index.js
@@ -1,24 +1,9 @@
+const getDBSession = require('remote').require('./src/db/sqlectron-db').getDBSession;
+
+
 /**
- * Important:
- * ----------
- * Since this module uses APIs not available in the render process.
- * To use it you must require it through the remote module.
- * https://github.com/atom/electron/blob/master/docs/api/remote.md
+ * Exposes sqlectron-db module from main process to render process
  */
-import conn from 'sqlectron-db';
-
-
-let db = null;
-export function getDB () {
-  if (db) { return Promise.resolve(db); }
-  try {
-    return conn('pg', {
-      host: '10.10.10.10',
-      user: 'user',
-      password: 'password',
-      database: 'postgres',
-    });
-  } catch (e) {
-    console.error(e.message || e);
-  }
-}
+export default {
+  getDBSession
+};
