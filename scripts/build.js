48584bc5eac94e8ca4dd71e753643415ea5b0f2b
Max Claus Nunes <maxcnunes@gmail.com>
2015-10-01 20:56:08 -0300

    Simplify code to ignore files in release

--- scripts/build.js
+++ scripts/build.js
@@ -17,9 +17,26 @@ import webpackConfig from '../webpack.prod.config';
  * --all    build for all platforms (default=true)
  */
 const argv = require('minimist')(process.argv.slice(2), { boolean: ['all', 'v'] });
-const BUILD_PATH = join(__dirname, '../build');
-const RELEASE_PATH = join(__dirname, '../releases');
-const RESOURCES_PATH = join(__dirname, '../resources');
+const ROOT_PATH = join(__dirname, '..');
+const BUILD_PATH = join(ROOT_PATH, 'build');
+const RELEASE_PATH = join(ROOT_PATH, 'releases');
+const RESOURCES_PATH = join(ROOT_PATH, 'resources');
+
+
+/**
+ * Ignore anything that is not required in the release app
+ */
+function ignoreFilesInRelease() {
+  const include = [
+    'node_modules',
+    'build',
+    'package.json'
+  ];
+
+  return readdirSync(ROOT_PATH)
+    .filter(filename => !~include.indexOf(filename))
+    .map(filename => `/${escapeRegExp(filename)}($|/)`);
+}
 
 
 /**
@@ -36,12 +53,7 @@ const ELECTRON_PACKAGER_OPTS = {
   overwrite: true,
   dir: '.',
   out: RELEASE_PATH,
-  ignore: [
-    // ignores everything we don't need in the release app
-    readdirSync(join(__dirname, '..'))
-      .filter(filename => !~['node_modules', 'build', 'package.json'].indexOf(filename))
-      .map(filename => new RegExp(`^${escapeRegExp(filename)}$`))
-  ]
+  ignore: ignoreFilesInRelease()
 };
 
 
