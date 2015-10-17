77534393b554a88bcec339e8448d73717bc36a90
Max Claus Nunes <maxcnunes@gmail.com>
2015-10-17 20:25:52 -0300

    Add support to auto restart the electron app in dev

--- src/browser/main.js
+++ src/browser/main.js
@@ -1,7 +1,7 @@
 var devMode = (process.argv || []).indexOf('--dev') !== -1;
 if (devMode) {
   // enables ES6+ support
-  require('babel/register')();
+  require('babel/register');
 } else {
   // enables ES6+ support withouth depending on babel runtime
   require('babel/polyfill');
