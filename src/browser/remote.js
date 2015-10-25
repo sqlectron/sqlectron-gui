2e57baf86a96da1902353041bafa0e5188713187
Max Claus Nunes <maxcnunes@gmail.com>
2015-10-25 10:36:53 -0200

    Remove getDBSession
    In the new connection approach we don’t use it anymore

--- src/browser/remote.js
+++ src/browser/remote.js
@@ -1,5 +1,4 @@
 const remote = require('remote');
-// const getDBSession = remote.require('./db/sqlectron-db').getDBSession;
 const servers = remote.require('./services/servers');
 const db = remote.require('./services/db');
 
@@ -8,7 +7,6 @@ const db = remote.require('./services/db');
  * Exposes to the renderer process some API running on main process
  */
 export default {
-  getDBSession: () => Promise.resolve({}),
   services: {
     servers,
     db,
