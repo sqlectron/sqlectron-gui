acabb0ecc190e83329b9c11e0fa20e226dde0461
Max Claus Nunes <maxcnunes@gmail.com>
2015-10-25 23:29:55 -0200

    Rollback electron upgrade
    Is not working quite well the error handling from remote in the latest version

--- src/browser/main.js
+++ src/browser/main.js
@@ -11,3 +11,17 @@ if (devMode) {
 
 // starts the electron app
 require('./app');
+
+
+if (devMode) {
+  /**
+   * Terrible workaround for:
+   * https://github.com/atom/electron/issues/3089
+   */
+  var configEnumerable = {
+    configurable: true,
+    enumerable: true,
+  };
+  Object.defineProperty(Error.prototype, 'message', configEnumerable);
+  Object.defineProperty(Error.prototype, 'stack', configEnumerable);
+}
