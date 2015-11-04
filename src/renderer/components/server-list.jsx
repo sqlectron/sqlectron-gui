1f087e530508f612f97275eaff82a8cfad77479a
Max Claus Nunes <maxcnunes@gmail.com>
2015-11-04 21:43:37 -0200

    Fix problem rendering server item with same name

--- src/renderer/components/server-list.jsx
+++ src/renderer/components/server-list.jsx
@@ -39,7 +39,7 @@ export default class ServerList extends Component {
         {this.groupItemsInRows(servers).map((row, rowIdx) =>
           <div key={rowIdx} className="doubling four column row">
             {row.map(server =>
-              <div key={server.name} className="wide column">
+              <div key={`${server.name}_${server.client}`} className="wide column">
                 <div className="ui">
                   <ServerListItem
                     onConnectClick={() => onConnectClick(server) }
