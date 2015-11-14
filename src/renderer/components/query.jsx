31477015ebf98ec47affebcc6299d4506f96e53c
Max Claus Nunes <maxcnunes@gmail.com>
2015-11-14 21:15:03 -0200

    Remove useless css styles and div components

--- src/renderer/components/query.jsx
+++ src/renderer/components/query.jsx
@@ -5,14 +5,6 @@ import 'brace/mode/sql';
 import 'brace/theme/github';
 import QueryResult from './query-result.jsx';
 
-const STYLES = {
-  queryBox: {
-  },
-  resultBox: {
-    background: '#ececec',
-  },
-};
-
 export default class Query extends Component {
   static propTypes = {
     query: PropTypes.object.isRequired,
@@ -33,28 +25,26 @@ export default class Query extends Component {
     return (
       <div>
         <div>
-          <div style={STYLES.queryBox}>
-            <div className="ui segment">
-              <AceEditor
-                mode="sql"
-                theme="github"
-                name="querybox"
-                height="10em"
-                width="100%"
-                ref="queryBoxTextarea"
-                value={query.query}
-                editorProps={{$blockScrolling: true}}
-                onChange={debounce(onSQLChange, 300)}
-                />
-            </div>
-            <div className="ui secondary menu" style={{marginTop: 0}}>
-              <div className="right menu">
-                <div className="item">
-                  <div className="ui buttons">
-                    <button className="ui positive button" onClick={::this.onExecQueryClick}>Execute</button>
-                    <div className="or"></div>
-                    <button className="ui button" onClick={::this.onDiscQueryClick}>Discard</button>
-                  </div>
+          <div className="ui segment">
+            <AceEditor
+              mode="sql"
+              theme="github"
+              name="querybox"
+              height="10em"
+              width="100%"
+              ref="queryBoxTextarea"
+              value={query.query}
+              editorProps={{$blockScrolling: true}}
+              onChange={debounce(onSQLChange, 300)}
+              />
+          </div>
+          <div className="ui secondary menu" style={{marginTop: 0}}>
+            <div className="right menu">
+              <div className="item">
+                <div className="ui buttons">
+                  <button className="ui positive button" onClick={::this.onExecQueryClick}>Execute</button>
+                  <div className="or"></div>
+                  <button className="ui button" onClick={::this.onDiscQueryClick}>Discard</button>
                 </div>
               </div>
             </div>
@@ -63,14 +53,12 @@ export default class Query extends Component {
         {
           !query.isExecuting &&
           query.resultRows &&
-          <div style={STYLES.resultBox}>
-            <QueryResult
-              query={query.queryHistory[query.queryHistory.length - 1]}
-              fields={query.resultFields}
-              rows={query.resultRows}
-              rowCount={query.resultRowCount}
-              error={query.error} />
-          </div>
+          <QueryResult
+            query={query.queryHistory[query.queryHistory.length - 1]}
+            fields={query.resultFields}
+            rows={query.resultRows}
+            rowCount={query.resultRowCount}
+            error={query.error} />
         }
       </div>
     );
