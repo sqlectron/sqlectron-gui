acd670eb07f2ce5cb74aeb92b08fd2a8581d1cf8
Max Claus Nunes <maxcnunes@gmail.com>
2015-11-25 01:37:26 -0200

    Change comment for ES6 support

--- src/browser/main.js
+++ src/browser/main.js
@@ -1,11 +1,10 @@
 /* eslint no-var: 0, no-extend-native: 0, vars-on-top: 0 */
 var devMode = (process.argv || []).indexOf('--dev') !== -1;
 
+// enables ES6+ support
 if (devMode) {
-  // enables ES6+ support
   require('babel/register');
 } else {
-  // enables ES6+ support withouth depending on babel runtime
   require('babel/polyfill');
 }
 
