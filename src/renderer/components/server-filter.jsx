b2576ae4f1352f303b8eadc5d472cc5616184104
Max Claus Nunes <maxcnunes@gmail.com>
2016-03-04 20:17:04 -0300

    Fix filter of servers (close #95)

--- src/renderer/components/server-filter.jsx
+++ src/renderer/components/server-filter.jsx
@@ -1,23 +1,33 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { debounce } from 'lodash';
 
 
-const ServerFilter = ({ onFilterChange, onAddClick }) => {
-  return (
-    <div className="ui small action left icon input fluid"
-        style={{marginBottom: '1em', fontSize: '0.8em'}}>
-      <i className="search icon"></i>
-      <input type="text" placeholder="Search..." onChange={debounce(onFilterChange, 200)} />
-      <button className="ui button green" onClick={onAddClick}>Add</button>
-    </div>
-  );
-};
+export default class ServerFilter extends Component {
+  static propTypes = {
+    onFilterChange: PropTypes.func.isRequired,
+    onAddClick: PropTypes.func.isRequired,
+  };
 
+  componentWillMount () {
+    this.delayedCallback = debounce(this.props.onFilterChange, 200);
+  }
 
-ServerFilter.propTypes = {
-  onFilterChange: PropTypes.func.isRequired,
-  onAddClick: PropTypes.func.isRequired,
-};
+  onFilterChange (event) {
+    event.persist();
+    this.delayedCallback(event);
+  }
+
+  render () {
+    return (
+      <div className="ui small action left icon input fluid"
+            style={{marginBottom: '1em', fontSize: '0.8em'}}>
+        <i className="search icon"></i>
+        <input type="text" placeholder="Search..." onChange={::this.onFilterChange} />
+        <button className="ui button green" onClick={::this.props.onAddClick}>Add</button>
+      </div>
+    );
+  }
+}
 
 
 export default ServerFilter;
