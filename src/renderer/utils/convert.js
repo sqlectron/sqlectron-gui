ba2550e29814e6766fcbf4f1acf91d0307d9d89b
Max Claus Nunes <maxcnunes@gmail.com>
2017-01-27 22:55:18 -0200

    Convert ArrayBuffer
    Close #260

--- src/renderer/utils/convert.js
+++ src/renderer/utils/convert.js
@@ -20,17 +20,40 @@ export function valueToString(value) {
   if (value === null) {
     return 'NULL';
   }
+
   if (typeof value === 'boolean') {
     return value.toString();
   }
+
   if (!value) {
     return String(value);
   }
+
   if (value.toISOString) {
     return value.toISOString();
   }
+
   if (typeof value === 'object') {
+    if (isArrayBuffer(value)) {
+      return arrayBufferToString(value);
+    }
+
     return JSON.stringify(value);
   }
+
   return String(value);
+}
+
+function arrayBufferToString(buf) {
+  if (buf.length === 1) {
+    // Probably is a bit column
+    return String(buf[0]);
+  }
+  return buf.toString('utf-8');
+}
+
+// reference:
+// http://stackoverflow.com/a/21799845/1050818
+function isArrayBuffer(value) {
+  return value && value.buffer instanceof ArrayBuffer && value.byteLength !== undefined;
 }
