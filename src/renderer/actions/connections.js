f1925f605edcbbc0434b546441977333d73e3961
Tamara Howell <tamara.howell@hotmail.com>
2017-10-26 15:31:41 -0400

    Typo fix connections.js

--- src/renderer/actions/connections.js
+++ src/renderer/actions/connections.js
@@ -60,8 +60,8 @@ export function connect (id, databaseName, reconnecting = false, sshPassphrase) 
       server = sqlectron.servers.decryptSecrects(server, cryptoSecret);
 
       // Terrible workaround to avoid a state issue of data loading from the main process.
-      // For some reason chaging a value here in client from a data coming from the main process
-      // don't have any effect. We need to clone this data and use the new state.
+      // For some reason changing a value here in client from a data coming from the main process
+      // doesn't have any effect. We need to clone this data and use the new state.
       server = JSON.parse(JSON.stringify(server));
 
       defaultDatabase = sqlectron.db.CLIENTS.find(c => c.key === server.client).defaultDatabase;
