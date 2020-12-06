4171177fc228044c23c9390db5fa435adc0fe40a
Max Claus Nunes <maxcnunes@gmail.com>
2020-12-06 12:02:51 -0300

    Add basic SQL suggestions

--- src/renderer/components/QueryEditor.tsx
+++ src/renderer/components/QueryEditor.tsx
@@ -1,8 +1,14 @@
-import React, { MutableRefObject } from 'react';
+import React, { MutableRefObject, useEffect } from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 export interface QueryEditorProps {}
+
+declare global {
+  interface Window {
+    completionItemProvider: monaco.IDisposable;
+  }
+}
 
 export const QueryEditor = React.forwardRef<
   monaco.editor.IStandaloneCodeEditor,
@@ -10,6 +16,44 @@ export const QueryEditor = React.forwardRef<
 >((props, editorRef) => {
   const getEditor = () =>
     editorRef as MutableRefObject<monaco.editor.IStandaloneCodeEditor>;
+
+  const editorWillMount = (monaco: any) => {
+    // Register SQL suggestions based on:
+    // https://github.com/microsoft/monaco-languages/blob/master/src/sql/sql.ts
+    const lang = monaco.languages
+      .getLanguages()
+      .find(({ id }: { id: string }) => id === 'sql')
+      .loader();
+
+    lang.then((res: any) => {
+      if (window.completionItemProvider) {
+        window.completionItemProvider.dispose();
+      }
+
+      window.completionItemProvider = monaco.languages.registerCompletionItemProvider(
+        'sql',
+        {
+          provideCompletionItems: function (model: any, position: any) {
+            // NOTE: The suggestions won't work after the first
+            // used suggestion if the suggestions object isn't re-created.
+            // That is why it is defined inside the provideCompletionItems
+            // instead of re-using a pre initialized variable.
+            const suggestions = res.language.keywords.map(
+              (keyword: string) => ({
+                label: keyword,
+                kind: monaco.languages.CompletionItemKind.Keyword,
+                insertText: keyword,
+              }),
+            );
+
+            return {
+              suggestions,
+            };
+          },
+        },
+      );
+    });
+  };
 
   const editorDidMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
     // Make editor accessible by parent components.
@@ -41,6 +85,7 @@ export const QueryEditor = React.forwardRef<
   const options = {
     selectOnLineNumbers: true,
     automaticLayout: false,
+    //quickSuggestions: true,
   };
 
   return (
@@ -48,6 +93,7 @@ export const QueryEditor = React.forwardRef<
       language='sql'
       theme='vs-dark'
       options={options}
+      editorWillMount={editorWillMount}
       editorDidMount={editorDidMount}
     />
   );
