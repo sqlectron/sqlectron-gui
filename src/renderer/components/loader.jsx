e713c0b61a39501b4e02916a0b5c9f108cc559ce
Matthew Peveler <matt.peveler@gmail.com>
2021-02-27 21:55:16 -0500

    Convert Loader component to function with hooks
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

--- src/renderer/components/loader.jsx
+++ src/renderer/components/loader.jsx
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
-export default class Loading extends Component {
-  static propTypes = {
-    message: PropTypes.string,
-    type: PropTypes.string,
-    inverted: PropTypes.bool,
-  };
+const Loader = ({ message, type, inverted }) => {
+  const ref = useRef(null);
 
-  componentDidMount() {
-    $(this.refs.loader).dimmer('show');
-  }
+  useEffect(() => {
+    $(ref.current).dimmer('show');
+    return () => $(ref.current).dimmer('hide');
+  }, []);
 
-  componentWillUnmount() {
-    $(this.refs.loader).dimmer('hide');
-  }
+  return (
+    <div className={`ui ${type} ${inverted ? 'inverted' : ''} dimmer`} ref={ref}>
+      <div className={`ui ${message ? 'text' : ''} loader`}>{message}</div>
+    </div>
+  );
+};
 
-  render() {
-    const { message, type } = this.props;
-    const inverted = this.props.inverted ? 'inverted' : '';
-    return (
-      <div className={`ui ${type} ${inverted} dimmer`} ref="loader">
-        <div className={`ui ${message ? 'text' : ''} loader`}>{message}</div>
-      </div>
-    );
-  }
-}
+Loader.propTypes = {
+  message: PropTypes.string,
+  type: PropTypes.string,
+  inverted: PropTypes.bool,
+};
+
+Loader.displayName = 'Loader';
+
+export default Loader;
