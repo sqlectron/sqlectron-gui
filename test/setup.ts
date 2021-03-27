9d8e6729f113da0aa3de7cc85c2879f7201e76ea
Matthew Peveler <matt.peveler@gmail.com>
2021-03-26 23:01:33 -0400

    add unit test of checkbox component
    Signed-off-by: Matthew Peveler <matt.peveler@gmail.com>

---
+++ test/setup.ts
@@ -0,0 +1,28 @@
+import { JSDOM } from 'jsdom';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target),
+  });
+}
+
+(global as any).window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+} as Navigator;
+global.requestAnimationFrame = function (callback) {
+  return setTimeout(callback, 0);
+};
+global.cancelAnimationFrame = function (id) {
+  clearTimeout(id);
+};
+copyProps(window, global);
